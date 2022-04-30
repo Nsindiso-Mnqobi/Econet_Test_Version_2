dc0e2e0e04366dc1aeb6a5bf8001f971faac128a
Nsindiso Mnqobi Matshanga <nsindiso.matshanga@econet.co.zw>
2022-04-30 06:24:54 +0000

    Created DB

--- eco_location.py
+++ eco_location.py
@@ -1,10 +1,15 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+#Create a flask instance 
 app = Flask(__name__)
 
-@app.route('/')
-def first_app():
-    return {'hello': 'world'}
+#Add Database 
+app.config('SQLALCHEMY_DATABASE_URI') = "mysql://root:root@localhost:3306/Area"
+
+#Intilaize The Database 
+db = SQLAlchemy()
 
 
 if  __name__ == "__main__":
