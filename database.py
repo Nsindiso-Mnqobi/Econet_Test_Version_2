10bd312aa22301a1e039080c3f2bd7c9b072d065
Nsindiso Mnqobi Matshanga <nsindiso.matshanga@econet.co.zw>
2022-04-30 08:42:23 +0000

    Editing code to  meet PEP8 Statndard

--- database.py
+++ database.py
@@ -2,23 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-#Create a flask instance 
+# Create a flask instance
 app = Flask(__name__)
-#Add Database 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@172.17.0.2:3306/Area"
+# Add Database
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql://root:root@172.17.0.2:3306/Area"
 
-#Intilaize The Database 
+# Intilaize The Database
 db = SQLAlchemy(app)
 
-#Create a Model 
+
+# Create a Model
 class Users(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     area = db.Column(db.String(100), unique=True, nullable=False)
     shop = db.Column(db.String(200), unique=True, nullable=False)
 
-    #Create A string
+    # Create A string
     def __repr__(self):
-        return '<Users %r>' % self.area
+        return "<Users %r>" % self.area
 
-if  __name__ == "__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
