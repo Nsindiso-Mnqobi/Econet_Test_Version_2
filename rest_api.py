e93b66572bf529843639caa26489638af37e126d
Nsindiso Mnqobi Matshanga <nsindiso.matshanga@econet.co.zw>
2022-04-30 19:15:05 +0000

    Cleaned Code

--- rest_api.py
+++ rest_api.py
@@ -1,72 +1,71 @@
-from email import message
-from re import S
-from flask import Flask
 from database import Users, db, app
 from flask_restx import Resource, Api, reqparse, abort, fields
 
-# Instantiate API 
+# Instantiate API
 api = Api(app)
 
-# Create Variables 
+# Create Variables
 Area = ""
 Shop = ""
 
 # Create Parser for delete and Post request
 parser = reqparse.RequestParser()
-parser.add_argument('Area',required=True,
-                                help="Area cannot be blank!")
-parser.add_argument('Shop',required=True,
-                                help="Shop cannot be blank!")
+parser.add_argument("Area", required=True, help="Area cannot be blank!")
+parser.add_argument("Shop", required=True, help="Shop cannot be blank!")
 
-# Create Parser for GET Request 
+# Create Parser for GET Request
 parser_2 = reqparse.RequestParser()
-parser_2.add_argument('Area',required=True,
-                                help="Area cannot be blank!")
+parser_2.add_argument("Area", required=True, help="Area cannot be blank!")
 
-Area = api.model('Area' , {'Area' : fields.String('Harare CBD')})
-Shop = api.model('Shop' , {'Area' : fields.String('Harare CBD') , 'Shop' : fields.String('Joina')})
+Area = api.model("Area", {"Area": fields.String("Harare CBD")})
+Shop = api.model(
+    "Shop",
+    {"Area": fields.String("Harare CBD"), "Shop": fields.String("Joina")},
+)
 
-# Create Class 
+
+# Create Class
 class Location(Resource):
 
-    # Get Request 
+    # Get Request
     @api.expect(Area)
     def get(self):
         args = parser_2.parse_args()
-        Area = args['Area']
+        Area = args["Area"]
         shop_list = []
         Shops = Users.query.filter_by(area=Area).all()
         for shop in Shops:
             if shop.area == Area:
                 shop_list.append(shop.shop)
-            elif Area != shop.area and shop.area == None:
-                abort(404, message= "The area is not avalaible in the system")
-            else: 
-                abort(201,message=" No Shops in this area")
-        return { "Shops" : shop_list}
+            elif shop.area == None:
+                abort(404, message="The area is not avalaible in the system")
+            else:
+                abort(201, message=" No Shops in this area")
+        return {"Shops": shop_list}
 
     # Put Request
-    @api.expect(Shop) 
+    @api.expect(Shop)
     def put(self):
-            args = parser.parse_args()
-            Area = args['Area']
-            Shop = args['Shop']
-            data = Users(area =Area, shop=Shop)
-            db.session.add(data)
-            db.session.commit()
-            return { "Area" : Area, "Shop" : Shop}
-    
-    # Delete Request 
+        args = parser.parse_args()
+        Area = args["Area"]
+        Shop = args["Shop"]
+        data = Users(area=Area, shop=Shop)
+        db.session.add(data)
+        db.session.commit()
+        return {"Area": Area, "Shop": Shop}
+
+    # Delete Request
     @api.expect(Shop)
     def delete(self):
-            args = parser.parse_args()
-            Area = args['Area']
-            Shop = args['Shop']
-            to_delete = Users.query.filter_by(shop=Shop).delete()
-            db.session.commit()
-            return { "Area" : Area, "Deleted Shop" : Shop}
+        args = parser.parse_args()
+        Area = args["Area"]
+        Shop = args["Shop"]
+        Users.query.filter_by(shop=Shop).delete()
+        db.session.commit()
+        return {"Area": Area, "Deleted Shop": Shop}
 
-api.add_resource(Location, '/location')
+
+api.add_resource(Location, "/location")
 
 if __name__ == "__main__":
     app.run(debug=True)
